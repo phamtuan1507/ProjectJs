423678d18d4fd65b00cc555a1502583133d7798a
pham <tuannp@vmms.com.vn>
2023-03-28 14:41:23 +0700

    git init

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+// đọc qua cái js es6
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
