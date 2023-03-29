57c200446be8e12112f76e230e7e70d42405483e
pham <tuannp@vmms.com.vn>
2023-03-29 16:48:13 +0700

    add

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express')
+const morgan = require('morgan')
+const handle = require('express-handlebars')
+
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')))
+// HTTP logger
+app.use(morgan('combined'))
+
+//Template engine
+app.engine('hbs', handle.engine({
+  extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
+// app.set('views', './views')
+app.set('views', path.join(__dirname, 'resources/views'));
+
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+app.get('/tin-tuc', (req, res) => {
+  res.render('news')
+})
+app.get('/tim-kiem', (req, res) => {
+  res.render('search')
+})
+app.post('/tim-kiem', (req, res) => {
+  // console.log(req.body)
+  // console.log(req.query)
+
+  res.send('')
+})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
